921704df7110fd59b88f0735bb796610784c00b7
Fozle Rabbi <fozlerabbi9790@gmail.com>
2023-07-24 16:22:51 +0600

    added user api and update favourite API , filter fabvourite data email wise

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
@@ -15,7 +16,6 @@ const datas = require("./Data/spatiolData.json")
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://KidsLand:2kMDcm0RtuViiUH1@kidsland.c47qxlr.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,13 +32,33 @@ async function run() {
     // await client.connect();
 
     const kidsSpecialCollection = client.db("kedsCollection").collection("spacialCollection");
+    const FavouriteCollection = client.db("kedsCollection").collection("FavouriteCollection");
+    const usersDataCollection = client.db("kedsCollection").collection("userCollection");
 
 
+    //==============================================>>>>>> users data collection Api from register
+    app.post("/users", async (req, res) => {
+      const usersData = req.body;
+      console.log(usersData);
+      const query = { email: usersData.email };
+      const existingUser = await usersDataCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" })
+      }
+      else {
+        const result = await usersDataCollection.insertOne(usersData)
+        res.send(result)
+      }
+    })
+
+
+    //==================================================>>>>>>>>>> this dynamic API for spacial collention section
     app.get("/specialDats/:gender", async (req, res) => {
       const gender = req.params.gender;
       if (gender === "all") {
         const result = await kidsSpecialCollection.find().toArray();
         res.send(result)
+        return
       }
       else {
         const query = { gender: gender }
@@ -48,7 +68,45 @@ async function run() {
 
     })
 
+    // ===================================================>>>>>>>>>>>>>>>>>>>> this API ADD favourite data coluction
+    app.post("/favouriteProducts", async (req, res) => {
+      const favouriteData = req.body;
+      const result = await FavouriteCollection.insertOne(favouriteData)
+      res.send(result)
+    });
 
+
+    // ======================================================>>>>>>>>>>>>>>>>>>>> this API GET favourite data coluction
+    app.get("/favouriteProducts", async (req, res) => {
+      const email = req?.query?.email;
+      const query = {email : email};
+      const result = await FavouriteCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    // ============================>>>>>>>>>>>>>>just for practice,,,,,,,,,,,,,,,,,,,,  ======= এক cullection থেকে Id এনে অন্য cullection এ filter করে , তার সাথে  index number add করে তার পর data পাঠান হয়েছে,,,
+    // app.get("/favouriteProducts", async (req, res) => {
+    //   const FavouriteCollectionData = await FavouriteCollection.find().toArray();
+    //   const query = { _id: { $in: FavouriteCollectionData.map(id => new ObjectId(id.productId)) } };
+    //   const indexNumber = FavouriteCollectionData.map(imgIndex => imgIndex.imageIndexNumber)
+
+    //   const kidsSpecialCollectionData = await kidsSpecialCollection.find(query).toArray();
+    //   const updatedKidsSpecialCollectionData = kidsSpecialCollectionData.map((doc, index) => ({
+    //     ...doc,
+    //     indexNumber: indexNumber[index]
+    //   }));
+    //   res.send(updatedKidsSpecialCollectionData)
+    // })
+
+
+
+    //=================================================>>>>>>>>>> This Api cunnect to NAV and delete favourite itemes 
+    app.delete("/favouriteProducts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await FavouriteCollection.deleteOne(query);
+      res.send(result)
+    })
 
 
 
